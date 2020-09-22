a33e1fc6cb75c81bbf494d1e0dde9d4b001390c3
Deborah Evelyn Carlota <evelyndec92@gmail.com>
2020-09-22 11:13:04 -0300

    Aula de MySQL

---
+++ Deborah-aula22-09-DML.sql
@@ -0,0 +1,20 @@
+create database if not exists db_deletar;
+use db_deletar;
+
+#drop table consumidores;
+create table if not exists consumidores (
+id int PRIMARY KEY AUTO_INCREMENT,
+nome varchar(30)
+);
+
+insert into consumidores (nome) values
+("Marcelo"),
+("Julia");
+
+select * from consumidores;
+
+delete from consumidores where id = 1;
+
+select * from consumidores;
+
+truncate consumidores;
