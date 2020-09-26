09c9e1aec04d500422bb62f27d775e0e6486a61d
Deborah Evelyn Carlota <evelyndec92@gmail.com>
2020-09-26 11:14:28 -0300

    Atualização do exercicio Game

--- Exercicio Game-25-09.sql
+++ Exercicio Game-25-09.sql
@@ -3,13 +3,26 @@ create database db_generation_game_online;
 
 use db_generation_game_online;
 
-create table tb_classe (
-id_classe int primary key auto_increment,
-mago varchar (255),
-necromante varchar (255),
-invocador varchar (255)
+create table tb_personagem (
+id_personagem int auto_increment,
+nome varchar (20),
+poderAtaque int,
+poderDefesa int,
+altura decimal (3,2),
+forca int,
+primary key (id_personagem)
 
 );
+
+
+create table tb_classe (
+id_classe int auto_increment,
+mago varchar (255),
+necromante varchar (255),
+invocador varchar (255),
+primary key (id_classe),
+id_personagem int,
+constraint fk_chave foreign key (id_personagem) references tb_personagem(id_personagem));
 
 INSERT INTO `db_generation_game_online`.`tb_classe`
 (`mago`,
@@ -21,17 +34,6 @@ VALUES
 ('Transmutação', 'aprisionar Alma', 'especialista em falar com animais');
 
 
-create table tb_personagem (
-id_personagem int primary key auto_increment,
-nome varchar (20),
-poderAtaque int,
-poderDefesa int,
-altura decimal (3,2),
-forca int,
-
-constraint fk_tb_classe foreign key (id_classe) references tb_classe(id_classe));
-
-
 INSERT INTO `db_generation_game_online`.`tb_personagem`
 (`nome`,
 `poderAtaque`,
@@ -41,20 +43,18 @@ INSERT INTO `db_generation_game_online`.`tb_personagem`
 
 VALUES
 ('Akim', 500, 1200, 1.90, 6),
-('Konos', 1000, 1200, 1.80, 7),
+('Konos', 1000, 1500, 1.80, 7),
 ('Claus', 2000, 1200, 1.95, 8),
 ('Hebreu', 1500, 1200, 1.85, 8),
-('Karma', 3000, 1200, 1.50, 10);
+('Karma', 3000, 1800, 1.50, 10);
 
 
 SELECT nome, poderAtaque from tb_personagem WHERE poderAtaque > (select max(2000) from tb_personagem);
-SELECT nome, poderDefesa from tb_personagem WHERE poderDefesa > (1000) > num_min AND (2000) < num_max;
-
-select * from tb_personagem WHERE nome LIKE '%C';
-
-select * from tb_personagem inner join tb_classe on tb_personagem.id_classe = tb_classe.id_classe;
+SELECT nome, poderDefesa from tb_personagem WHERE poderDefesa > 1000 AND 2000 < poderDefesa;
 
 
+select * from tb_personagem WHERE nome LIKE 'C%';
 
+select * from tb_personagem inner join tb_classe on tb_personagem.id_personagem = tb_classe.id_classe;
 
 
